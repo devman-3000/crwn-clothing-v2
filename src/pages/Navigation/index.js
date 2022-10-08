35996fb907e05a0e5343aee586efe05b6b57c3cb
Nikolay M <nikolay.m@MacBook-Pro-Blaeberry.local>
2022-10-08 20:12:18 +0300

    lesson 9

--- src/pages/Navigation/index.js
+++ src/pages/Navigation/index.js
@@ -1,12 +1,15 @@
 import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
 
 const Navigation = () => {
     return (
       <>
         <div className="navigation">
-            <Link className="logo-container" to='/'><div>Logo</div> 
+            <Link className="logo-container" to='/'>
+                <CrownLogo className='logo' /> 
             </Link>
-            <div className="links-container">
+            <div className="nav-links-container">
                 <Link className="nav-link" to="/shop">Shop</Link>
             </div>   
         </div>
